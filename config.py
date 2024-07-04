c3c2f081b84f9c837c24159cc3b61728707968b1
Ravi Singh <ravi149185@gmail.com>
2024-07-05 02:18:49 +0530

    chore: Refactor config.py for better configuration management

--- config.py
+++ config.py
@@ -1,20 +1,22 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
   GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+  CONFIG_MODE = os.getenv('CONFIG_MODE')
   UPLOAD_FOLDER = 'uploads/'
   ALLOWED_EXTENSIONS = {'pdf'}
-  SECRET_KEY = os.getenv('SECRET_KEY') | 'secret_key'
+  SECRET_KEY = os.getenv('SECRET_KEY') or 'secret_key'
   SESSION_TYPE = 'filesystem'
-  SESSION_COOKIE_NAME = os.getenv('SESSION_COOKIE_NAME') | 'your_session_cookie_name'
+  SESSION_COOKIE_NAME = os.getenv('SESSION_COOKIE_NAME') or 'your_session_cookie_name'
   SESSION_COOKIE_HTTPONLY = True
   SESSION_COOKIE_SECURE = True
   SESSION_COOKIE_MAX_SIZE = 4093
 
 class DevelopmentConfig(Config):
-  DEVELOPMENT = True
   DEBUG = True
 
-config = {
-  "development": DevelopmentConfig,
-}
+class ProductionConfig(Config):
+  DEBUG = False
