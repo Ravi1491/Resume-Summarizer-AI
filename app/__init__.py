cbcd0cd4a9f4622f778fc02a611bd23c16d27232
Ravi Singh <ravi149185@gmail.com>
2024-07-23 16:16:41 +0530

    chore: Update app initialization to include new routes and models for dashboard functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
 from flask_session import Session
 from config import DevelopmentConfig, ProductionConfig
-import os
+
+from .models import init_db
+from .routes.index import app as app_blueprint
+from .routes.auth import auth as auth_blueprint
+from .routes.dashboard import dashboard as dashboard_blueprint
 
 def create_app():
     app = Flask(__name__)
@@ -18,12 +23,10 @@ def create_app():
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
 
-    from .routes.index import app as routes_blueprint
-    from .routes.auth import auth as auth_blueprint
-    app.register_blueprint(routes_blueprint)
+    app.register_blueprint(app_blueprint)
     app.register_blueprint(auth_blueprint)
+    app.register_blueprint(dashboard_blueprint)
 
-    from .models import init_db
     init_db()
     
     return app
---
+++ app/routes/dashboard.py
@@ -0,0 +1,110 @@
+from flask import render_template, request, redirect, flash, url_for, Blueprint, current_app
+from werkzeug.utils import secure_filename
+from ..models import get_all_pdfs, delete_pdf_entry, get_pdf_by_id
+from ..utils import get_resume_pdf_text, generate_text, allowed_file,match_job_description
+import os
+import sqlite3
+import json
+from ..utils import dict_to_html_table
+
+dashboard = Blueprint('dashboard', __name__)
+
+@dashboard.route('/dashboard', methods=['POST', 'GET'])
+def get_all_resumes():
+    uploaded_pdfs = get_all_pdfs()
+    return render_template('dashboard.html', uploaded_pdfs=uploaded_pdfs)
+
+@dashboard.route('/compare', methods=['POST', 'GET'])
+def compare():
+    if request.method == 'POST' and 'job_description' in request.form:
+        allPdfs = get_all_pdfs()
+        final_res = []
+        
+        for pdf in allPdfs:
+            res = match_job_description(file_name=pdf[1],ai_text=pdf[3], job_description=request.form['job_description'])
+            santize_res = json.loads(res)
+            final_res.append(santize_res)
+        
+        data = {
+            'job_description': request.form['job_description'],
+            'comparison_results': final_res
+        }
+        return render_template('compare.html', data=data)
+        
+    return render_template('compare.html')
+
+@dashboard.route('/view/<int:id>')
+def view_pdf(id):
+    pdf = get_pdf_by_id(id)
+    if pdf:
+        content = json.loads(pdf[2])
+        html_table = dict_to_html_table(content)
+        return render_template('view.html', pdf=html_table)
+    else:
+        flash('PDF not found')
+        return redirect(url_for('app.index'))
+
+@dashboard.route('/delete/<int:id>', methods=['POST'])
+def delete_pdf(id):
+    delete_pdf_entry(id)
+    return redirect(url_for('app.dashboard'))
+
+@dashboard.route('/upload', methods=['POST'])
+def upload_file():
+    if 'pdfs' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+
+    files = request.files.getlist('pdfs')
+    saved_files = []
+
+    for file in files:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            saved_files.append(filename)
+            with sqlite3.connect('database.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute('INSERT INTO pdfs (filename) VALUES (?)', (filename,))
+                conn.commit()
+        else:
+            flash(f'File {file.filename} is not a valid PDF')
+
+    if saved_files:
+        flash(f'Successfully uploaded: {", ".join(saved_files)}')
+        all_pdfs = get_all_pdfs()
+
+        for pdf in all_pdfs:
+            filename = pdf[1]
+            text = get_resume_pdf_text(f'uploads/{filename}')
+            prompt = f"""You are Experience Software Developer. You will have to make summary of resumes section wise.
+            Your role is to summarize the resume in a way that it is easy to read and understand.
+            Divide the summary in following sections:
+            1. Personal_Information
+            2. Objective
+            3. Work Experience
+            4. Education
+            5. Skills
+            6. Projects
+            7. Certifications
+            8. Hobbies
+            9. Languages
+            10. References
+
+            If some section is missing, you can skip that section.
+            Only give the response in JSON format.
+            Don't give any text outside the JSON. Only provide JSON. 
+            Even Don't include Here is the summary of the resume in JSON format: 
+            Here is the text from the resume: {text}"""
+
+            generated_text = generate_text(prompt, text)
+
+            with sqlite3.connect('database.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute('UPDATE pdfs SET text = ?, ai_text = ? WHERE id = ?', (text, generated_text, pdf[0]))
+                conn.commit()
+            flash(generated_text)
+    else:
+        flash('No valid files uploaded')
+
+    return redirect(url_for('app.dashboard'))
